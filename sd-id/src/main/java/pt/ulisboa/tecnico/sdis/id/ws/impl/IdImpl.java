8021d149915c6b3ea152447d8222d2591745c357
Gabriel Freire <gabriel.freire@tecnico.ulisboa.pt>
2015-04-08 16:05:01 +0100

    First Commit

---
+++ sd-id/src/main/java/pt/ulisboa/tecnico/sdis/id/ws/impl/IdImpl.java
@@ -0,0 +1,41 @@
+package pt.ulisboa.tecnico.sdis.id.ws.impl;
+
+import javax.jws.*;
+
+import pt.ulisboa.tecnico.sdis.id.ws.*; // classes generated from WSDL
+
+@WebService(
+		endpointInterface="pt.ulisboa.tecnico.sdis.id.ws.SDId", 
+	    wsdlLocation="SD-ID.1_1.wsdl",
+	    name="SdId",
+	    portName="SDIdImplPort",
+	    targetNamespace="urn:pt:ulisboa:tecnico:sdis:id:ws",
+	    serviceName="SDId"
+)
+public class IdImpl implements SDId {
+
+
+	public void createUser(String userId, String emailAddress)
+			throws EmailAlreadyExists_Exception, InvalidEmail_Exception,
+			InvalidUser_Exception, UserAlreadyExists_Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void renewPassword(String userId) throws UserDoesNotExist_Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removeUser(String userId) throws UserDoesNotExist_Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public byte[] requestAuthentication(String userId, byte[] reserved)
+			throws AuthReqFailed_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
