0f3dba29ea620faef71cda4f3e84e823f27e3314
Gabriel Freire <gabriel.freire@tecnico.ulisboa.pt>
2015-04-16 18:25:55 +0100

    primeiro teste atualizado
    ainda está em progresso, é só para terem noção do que estou a fazer ahah

--- sd-id/Projeto_cliente/src/test/CreateUserTest.java
+++ sd-id/Projeto_cliente/src/test/CreateUserTest.java
@@ -1,6 +1,8 @@
-package pt.ulisboa.tecnico.sdis.id.ws.test;
+package pt.ulisboa.tecnico.sdis.id.ws;
 
 import org.junit.*;
+
+import pt.ulisboa.tecnico.sdis.id.ws.CreateUser;
 import static org.junit.Assert.*;
 
 /**
@@ -25,27 +27,59 @@ public class CreateUserTest {
 
 
     // members
-
+    private CreateUser user1, user2, user3, user4, user5;
 
     // initialization and clean-up for each test
 
     @Before
     public void setUp() {
     	
+    	user1 = new CreateUser();
+    	user2 = new CreateUser();
+    	user3 = new CreateUser();
+    	user4 = new CreateUser();
+    	user5 = new CreateUser();
+    	
     }
 
     @After
     public void tearDown() {
+    	
+    	user1 = null;
+    	user2 = null;
+    	user3 = null;
+    	user4 = null;
+    	user5 = null;
+    	
     }
 
 
     // tests
+		// assertEquals(expected, actual);
+    	// if the assert fails, the test fails
 
-    @Test
-    public void test() {
-
-        // assertEquals(expected, actual);
-        // if the assert fails, the test fails
+    @Test(expected = InvalidUser_Exception.class)
+    public void testSetIdNull() {
+    	user1.setUserId("");    
     }
-
+    @Test(expected = InvalidUser_Exception.class)
+    public void testSetIdInt() {
+    	user1.setUserId("5");    
+    }
+    
+    @Test(expected = InvalidEmail_Exception.class)
+    public void testSetEmailAddress1() {
+    	user1.setEmailAddress("aloalo");
+    }
+    @Test(expected = InvalidEmail_Exception.class)
+    public void testSetEmailAddress2() {
+    	user1.setEmailAddress("@aloalo");
+    }
+    
+    @Test(expected = InvalidEmail_Exception.class)
+    public void testSetEmailAddress3() {
+    	user1.setEmailAddress("aloalo@");
+    }
+    
+    
 }
